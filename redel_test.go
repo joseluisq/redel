c4faf791d4ccded95c4416856637925c48ec7de8
Jose Quintana <joseluisquintana20@gmail.com>
2019-06-04 02:18:13 +0200

    chore: add more test cases

--- redel_test.go
+++ redel_test.go
@@ -1,6 +1,7 @@
 package redel
 
 import (
+	"bytes"
 	"strings"
 	"testing"
 )
@@ -49,6 +50,88 @@ func TestReplaceFilterString(t *testing.T) {
 	}, filterFunc, false)
 
 	if output != expectedStr {
-		t.Fatal("(ReplaceFilter) Failed to match strings!")
+		t.Fatal("(ReplaceFilter with no preserve delimiters) Failed to match strings!")
+	}
+}
+
+func TestReplaceFilterPreserveString(t *testing.T) {
+	r := strings.NewReader(str)
+
+	rep := New(r, []Delimiter{
+		{Start: []byte("("), End: []byte(")")},
+	})
+
+	expectedStr := "(REPLACEMENT) ipsum dolor (REPLACEMENT) magna (REPLACEMENT) varius (REPLACEMENT)."
+	replacement := []byte("REPLACEMENT")
+
+	output := ""
+
+	filterFunc := func(matchValue []byte) bool {
+		return true
+	}
+
+	rep.ReplaceFilter(replacement, func(data []byte, atEOF bool) {
+		output = output + string(data)
+	}, filterFunc, true)
+
+	if output != expectedStr {
+		t.Fatal("(ReplaceFilter + preserve delimiters) Failed to match strings!")
+	}
+}
+func TestReplaceFilterWithString(t *testing.T) {
+	r := strings.NewReader(str)
+
+	rep := New(r, []Delimiter{
+		{Start: []byte("("), End: []byte(")")},
+	})
+
+	expectedStr := "Lorem (  ipsum dolor CUSTOM magna  suscipit.  varius CUSTOM."
+	replaceWithThis := []byte("CUSTOM")
+
+	output := ""
+
+	filterFunc := func(matchValue []byte) []byte {
+		if bytes.Equal(matchValue, []byte(" sapien ")) || bytes.Equal(matchValue, []byte(" nam risus ")) {
+			return replaceWithThis
+		}
+
+		return matchValue
+	}
+
+	rep.ReplaceFilterWith(func(data []byte, atEOF bool) {
+		output = output + string(data)
+	}, filterFunc, false)
+
+	if output != expectedStr {
+		t.Fatal("(ReplaceFilterWith + no preserve delimiters) Failed to match strings!")
+	}
+}
+func TestReplaceFilterWithPreserveString(t *testing.T) {
+	r := strings.NewReader(str)
+
+	rep := New(r, []Delimiter{
+		{Start: []byte("("), End: []byte(")")},
+	})
+
+	expectedStr := "(Lorem ( ) ipsum dolor ( nam risus ) magna ( suscipit. ) varius (CUSTOM)."
+	replaceWithThis := []byte("CUSTOM")
+	hasThisValue := []byte(" sapien ")
+
+	output := ""
+
+	filterFunc := func(matchValue []byte) []byte {
+		if bytes.Equal(matchValue, hasThisValue) {
+			return replaceWithThis
+		}
+
+		return matchValue
+	}
+
+	rep.ReplaceFilterWith(func(data []byte, atEOF bool) {
+		output = output + string(data)
+	}, filterFunc, true)
+
+	if output != expectedStr {
+		t.Fatal("(ReplaceFilterWith + preserve delimiters) Failed to match strings!")
 	}
 }
